c3c24023d1e37626604d401cc42b915887de230e
NicolasCeschi <66810576+NicolasCeschi@users.noreply.github.com>
2025-03-30 19:05:45 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/note-guru-sync/",  // 🔹 Agrega esta línea
 }));
